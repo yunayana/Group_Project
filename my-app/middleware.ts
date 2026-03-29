114b82dde9d109db05e7dd589bb1bb4ae1d876c8
yunayana <21232@student.ans-elblag.pl>
2026-03-29 21:48:21 +0200

    Remove middleware completely for debug

--- my-app/middleware.ts
+++
@@ -1,9 +0,0 @@
-import { NextResponse, type NextRequest } from "next/server";
-
-export function middleware(request: NextRequest) {
-  return NextResponse.next();
-}
-
-export const config = {
-  matcher: ["/:path*"],
-};
